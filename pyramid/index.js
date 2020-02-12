ad111e868a75a25dd26ce91ba27c9b656dce1021
Sarthak Chavan <sarthakchavan290195@gmail.com>
2020-02-12 15:48:51 -0500

    Done with Pyramid Recursive Solution

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,19 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
+function pyramid(n, row = 0, level = "") {
+  //Base case
+  if (row === n) return;
 
-  for (let row = 0; row < n; row++) {
-    let level = "";
-
-    for (let column = 0; column < 2 * n - 1; column++) {
-      if (midpoint - row <= column && midpoint + row >= column) level += "#";
-      else level += " ";
-    }
-
+  //Case to go to the next level
+  if (level.length === 2 * n - 1) {
     console.log(level);
+    //Start a new row
+    return pyramid(n, row + 1);
   }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+
+  //Decide to put a # or a _
+  if (midpoint - row <= level.length && midpoint + row >= level.length)
+    add = "#";
+  else add = " ";
+
+  //Same row, add characters
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) level += "#";
+//       else level += " ";
+//     }
+
+//     console.log(level);
+//   }
+// }
